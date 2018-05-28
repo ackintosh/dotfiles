df346cb24cf80900a3eaa5ae65b08ee9a280f749
ackintosh <sora.akatsuki@gmail.com>
2018-05-28 15:22:30 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+brew install git
+brew install jq
+brew install htop
+brew install tmux
+brew install reattach-to-user-namespace
+brew install go
+brew install ghq
+brew install peco
+brew install pt
+brew install direnv
+brew install vim --with-lua
+
