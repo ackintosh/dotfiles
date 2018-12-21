fe48d9f3b3f11c405d1c70d86ba7198a47a9306f
Akihito Nakano <sora.akatsuki@gmail.com>
2018-12-21 16:23:39 +0900

    Update put_symbolic_links.sh

--- setup/put_symbolic_links.sh
+++ setup/put_symbolic_links.sh
@@ -4,3 +4,12 @@ cd ~/.config/fish; ln -s ~/src/github.com/ackintosh/dotfiles/fish/config.fish
 cd ~/.config/fish/functions; ln -s ~/src/github.com/ackintosh/dotfiles/fish/fish_user_key_bindings.fish
 # gitignore_global
 cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.gitignore_global
+# tmux
+cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.tmux.conf
+# vim
+cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.vimrc
+cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.vim
+# IDEA
+cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.ideavimrc
+# Ruby
+cd ~; ln -s ~/src/github.com/ackintosh/dotfiles/.gemrc
