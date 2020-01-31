01c3cb73e77161a9f87d8b9e6b8f2594e3efea8f
akihito.nakano <sora.akatsuki@gmail.com>
2020-01-31 20:04:30 +0900

    Go

--- fish/config.fish
+++ fish/config.fish
@@ -20,6 +20,15 @@ if test -d ~/go/bin
   set PATH $PATH ~/go/bin
 end
 
+# Go
+# http://qiita.com/methane/items/d82b9f28b97b5c3bd08a
+# http://d.hatena.ne.jp/hiro_nemu/20140113/1389620004
+if which -s go
+  set GOROOT (go env GOROOT)
+  set GOPATH $HOME
+  set PATH $PATH:$GOROOT/bin:$GOPATH/bin:/usr/local/opt/go/libexec/bin
+end
+
 balias g 'git'
 balias gb 'git branch'
 balias gco 'git checkout'
@@ -40,6 +49,7 @@ balias ll 'ls -lh'
 balias 1 'cd -'
 balias 2 'cd -2'
 balias vi 'vim'
+balias python3 'python'
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f ~/google-cloud-sdk/path.fish.inc ]; if type source > /dev/null; source ~/google-cloud-sdk/path.fish.inc; else; . ~/google-cloud-sdk/path.fish.inc; end; end
